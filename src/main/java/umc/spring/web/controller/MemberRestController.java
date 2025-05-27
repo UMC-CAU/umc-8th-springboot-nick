51953f7268db4233cfe07c9eab38d51e64f22aa5
이석주 <nick981213@naver.com>
2025-05-28 01:57:31 +0900

    feat: 특정 가게의 미션 목록 조회 API 구현 (#11)

--- src/main/java/umc/spring/web/controller/MemberRestController.java
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,17 +1,27 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
 import umc.spring.converter.MemberMissionConverter;
+import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.Review;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.service.memberService.MemberCommandService;
+import umc.spring.service.storeService.StoreQueryService;
+import umc.spring.validation.annotation.ValidatePage;
 import umc.spring.web.dto.member.MemberRequestDTO;
 import umc.spring.web.dto.member.MemberResponseDTO;
 import umc.spring.web.dto.memberMission.MemberMissionResponseDTO;
+import umc.spring.web.dto.store.StoreResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +29,7 @@ import umc.spring.web.dto.memberMission.MemberMissionResponseDTO;
 public class MemberRestController {
 
     private final MemberCommandService memberCommandService;
+    private final StoreQueryService storeQueryService;
 
     @PostMapping("/")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
@@ -31,5 +42,18 @@ public class MemberRestController {
                                                                          @PathVariable Long missionId){
         MemberMission memberMission = memberCommandService.addMemberMission(memberId, missionId);
         return ApiResponse.onSuccess(MemberMissionConverter.toMemberMissionResponseDTO(memberMission));
+    }
+
+    @GetMapping("/{memberId}/reviews")
+    @Operation(summary = "특정 유저의 리뷰 목록 조회 API", description = "특정 유저의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공")
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "조회하고자 하는 member의 id, path variable 입니다!")
+    })
+    public ApiResponse<StoreResponseDTO.ReviewPreviewListDTO> getMemberReviewList(@PathVariable(name = "memberId") Long memberId, @ValidatePage @RequestParam(name = "page") Integer page) {
+        Page<Review> reviewList = storeQueryService.getMemberReviewList(memberId, page);
+        return ApiResponse.onSuccess(StoreConverter.reviewPreViewListDTO(reviewList));
     }
 }
--- src/main/java/umc/spring/web/dto/store/StoreResponseDTO.java
+++ src/main/java/umc/spring/web/dto/store/StoreResponseDTO.java
@@ -43,4 +43,28 @@ public class StoreResponseDTO {
         String body;
         LocalDate createdAt;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MissionPreviewListDTO{
+        List<MissionPreviewDTO> missionList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MissionPreviewDTO{
+        String missionSpec;
+        Integer reward;
+        LocalDate deadline;
+        LocalDateTime createdAt;
+    }
 }

9334f1d848409ddee25c71ce6a482a4d538be4e7
이석주 <nick981213@naver.com>
2025-05-28 01:11:53 +0900

    feat: week 9 실습 진행

--- src/main/java/umc/spring/converter/StoreConverter.java
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -1,8 +1,13 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StoreConverter {
 
@@ -17,6 +22,28 @@ public class StoreConverter {
         return StoreResponseDTO.AddResultDTO.builder()
                 .storeId(store.getId())
                 .createdAt(store.getCreatedAt())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreviewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreviewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreviewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+        List<StoreResponseDTO.ReviewPreviewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreviewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
--- src/main/java/umc/spring/service/storeService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/storeService/StoreQueryServiceImpl.java
@@ -1,9 +1,15 @@
 package umc.spring.service.storeService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.Member;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.MemberRepository;
+import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository;
 
 import java.util.List;
@@ -15,6 +21,8 @@ import java.util.Optional;
 public class StoreQueryServiceImpl implements StoreQueryService {
 
     private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public Optional<Store> findStore(Long id) {
@@ -29,4 +37,22 @@ public class StoreQueryServiceImpl implements StoreQueryService {
 
         return filteredStores;
     }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+        Store store = storeRepository.findById(StoreId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
+    }
+
+    @Override
+    public Page<Review> getMemberReviewList(Long MemberId, Integer page) {
+        Member member = memberRepository.findById(MemberId)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        return reviewRepository.findAllByMember(member, PageRequest.of(page, 10));
+    }
+
+
 }
